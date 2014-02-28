603070be385819d87843da50d9e713da4d682d7e
vegeta <vegeta@gmail.com>
2014-02-28 09:26:14 +0800

    Fix support page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ CaseJongchyi::Application.routes.draw do
   root "pages#home"    
   get "home", to: "pages#home", as: "home"
   get "inside", to: "pages#inside", as: "inside"
+  get "support", to: "pages#support", as: "support"
   
     
   devise_for :users, skip: :passwords, :controllers => { sessions: 'admin/sessions' }
